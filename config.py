f161dcb496bf065df584b10f47a4bbe29b73c177
MindMayhem <39557395+MindMayhem@users.noreply.github.com>
2018-06-30 12:39:33 +0300

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-import MySQLdb
-from telebot import apihelper
-
-token = "606278221:AAE4Q2TbDD3l2CfGiMMj2I2o7aG0n1tdrgs"
-proxy = apihelper.proxy = {'https': 'socks5://telegram:telegram@kwwbj.teletype.live:1080'} #подключение к прокси # подключение через прокси (конфиг)
